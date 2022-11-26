d717ecffcb02898f3f36fa38c54d15afbb3b88ba
giselearaujos <giseslearaujo07.s@@gmail.com>
2022-11-26 14:10:15 -0300

    x
    :recycle: Change de import images

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -3,7 +3,10 @@ import "./Banner.css";
 const Banner = () => {
   return (
     <header className="banner">
-      <img src="./public/banner.png" alt="Imagem inicial do projeto organo" />
+      <img
+        src="../../../public/banner.png"
+        alt="Imagem inicial do projeto organo"
+      />
     </header>
   );
 };
